03271cbec4780cf213bb6001f71619dec690cb61
Matias Lorenzo <matiaslorenzo@Matiass-MacBook-Pro.local>
2018-02-13 15:53:10 +0100

    remove sass files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Routes from './config/routes';
 
-import './index.scss';
-
 ReactDom.render(<Routes />, document.getElementById('app'));
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,6 +2,11 @@ import { injectGlobal } from 'styled-components';
 
 export default injectGlobal`
   body {
+    height: 100vh;
     font-family: 'Orbitron', sans-serif
   }
+  div {
+        width: 100%;
+        height:100%;
+    }
 `;
